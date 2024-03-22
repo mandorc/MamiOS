191c8dbfc4a4f9095d1516b5217fbcbcd6c01626
Armando <95893860+mandorc@users.noreply.github.com>
2024-03-22 10:39:01 -0600

    Update

---
+++ MamiOS/src/Main.java
@@ -0,0 +1,19 @@
+
+import Views.MainFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author armando
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
